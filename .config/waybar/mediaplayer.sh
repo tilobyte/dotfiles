eba2dc067053642f882cf7eac5b5c66e49da5953
Timothy Akintilo <timtobi1@gmail.com>
2020-10-16 00:17:00 -0700

    gruvboxify rofi and switch to kitty

--- .config/waybar/mediaplayer.sh
+++ .config/waybar/mediaplayer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 player_status=$(playerctl status 2> /dev/null)
-PLAYERCTL=$(echo "$(playerctl metadata artist) – $(playerctl metadata title)")
+PLAYERCTL=$(echo "$(playerctl --player=playerctld metadata artist) – $(playerctl --player=playerctld metadata title)")
 if [ "$player_status" = "Playing" ]; then
 	echo '{"text": " '$PLAYERCTL'", "tooltip": "'$PLAYERCTL'", "class": "playing" }'
 elif [ "$player_status" = "Paused" ]; then
