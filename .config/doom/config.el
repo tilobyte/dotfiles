d0e68359164b0ab01b51e55bc61995b6e68891bb
Timothy Akintilo <timtobi1@gmail.com>
2021-01-02 16:32:59 -0800

    back to windows?

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -22,6 +22,7 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 (setq doom-font (font-spec :family "Iosevka" :size 16))
+(setq doom-big-font (font-spec :family "Iosevka" :size 32))
 (setq doom-variable-pitch-font (font-spec :family "Cantarell" :size 14))
 ;; (setq doom-variable-pitch-font (font-spec :family "Iosevka" :size 16))
 
@@ -60,9 +61,7 @@
 ;; editor
 (setq evil-escape-key-sequence "vk")
 (setq which-key-idle-delay 0.3) ;; training wheels
-;; transparency (doesn't seem to work on wayland)
-;; (set-frame-parameter (selected-frame) 'alpha '(92 . 90))
-;; (add-to-list 'default-frame-alist '(alpha . (92 . 90)))
+(global-set-key (kbd "C-/") 'comment-line)
 
 ;; lsp
 (setq company-minimum-prefix-length 1
@@ -81,3 +80,50 @@
 (auctex-latexmk-setup)
 (company-auctex-init)
 (setq +latex-viewers '(zathura))
+;; https://github.com/c02y/dotemacs.d/blob/81153dcce335b0d2ef4b2416eae6e1ca3c70cd76/init.el#L2955-L3098
+(defun LaTeX-save-and-compile ()
+  (interactive)
+  (progn
+	;; ;; turn off smartparens because LaTeX-electric-left-right-brace
+	;; ;; offers more for specific LaTeX mode
+	;; ;; Since SP is always triggered later by sth., so put these two lines here
+	;; (turn-off-smartparens-mode)
+	;; (setq LaTeX-electric-left-right-brace t)
+	(let ((TeX-save-query nil)
+		  (TeX-process-asynchronous nil)
+		  (master-file (TeX-master-file)))
+	  (TeX-save-document "")
+	  ;; clean all generated files before compile
+	  ;; DO NOT do it when up-to-date, remove this line in proper time
+	  (TeX-clean t)
+	  (TeX-run-TeX "latexmk"
+				   (TeX-command-expand "latexmk -pdflatex='pdflatex -file-line-error -synctex=1' -pdf %s" 'TeX-master-file)
+				   master-file)
+	  (if (plist-get TeX-error-report-switches (intern master-file))
+		  ;; avoid creating multiple windows to show the *TeX Help* error buffer
+		  (if (get-buffer-window (get-buffer "*TeX Help*"))
+			  (TeX-next-error)
+			(progn
+			  (split-window-vertically -10)
+			  (TeX-next-error)))
+		;; if no errors, delete *TeX Help* window and buffer
+		(if (get-buffer "*TeX Help*")
+			(progn
+			  (if (get-buffer-window (get-buffer "*TeX Help*"))
+				  (delete-windows-on "*TeX Help*"))
+			  (kill-buffer "*TeX Help*")))))))
+(add-hook 'latex-mode-hook
+        (lambda ()
+                (add-hook 'after-save-hook 'LaTeX-save-and-compile nil 'make-it-local)))
+
+;; send literal key to term using C-`
+(defun singpolyma/term-insert-literal (key)
+    "Take a keypress and insert it literally into a terminal."
+    (interactive "cPress key:")
+    (term-send-raw-string (format "%c" key))
+    )
+
+(add-hook 'term-mode-hook (lambda ()
+        (define-key evil-insert-state-map (kbd "C-`") 'singpolyma/term-insert-literal)
+        )
+)
